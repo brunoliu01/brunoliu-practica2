84713e1dfe6978b5320250c453f5a545775c8b4f
Giancarlo Llerena <gian1234cv@gmail.com>
2016-10-21 09:53:21 -0500

    xdxxddx

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.getElementById("ejer-1").onclick = function() {hide()};
+
+
+function hide() {
+	document.getElementById("ejer-1").style.display = 'none';
+}
---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+function mostar() {
+	div = document.getElementById('h2');
+	 div.style.display = '';
+}
+
+function cerrar () {
+		div = document.getElementById('h2');
+	 div.style.display = 'none';
+}
